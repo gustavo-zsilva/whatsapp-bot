58c378ac659e45609aa90a25a8fc5f6a9ea55acd
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-05-24 12:02:26 -0300

    finished menu functionality

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, LocalAuth } from 'whatsapp-web.js'
+import { Client, LocalAuth, Message } from 'whatsapp-web.js'
 import qrcode from 'qrcode-terminal'
 require('dotenv').config()
 
@@ -7,20 +7,39 @@ const client = new Client({ authStrategy: new LocalAuth(), webVersionCache: {
     type: 'remote',
 } });
 
-let hasClientStartedChat = false
-let isOrderFinished = false
-let isOrderCancelled = false
+type Orders = {
+    chatId: {
+        name: string,
+        price: number,
+    }[]
+}
 
-const messagesList = [
-    {
-        senderMsg: '1',
-        responseMsg: 'Olá! Aqui é a vargas barbearia.'
+const ongoingChats = []
+const orders: Orders | {} = {}
+
+function handleSendMenu(chatId: string) {
+    // Fetch menu from an api
+    let textMenu = ''
+    menu.forEach((menuItem, index) => textMenu += `${index + 1}. ${menuItem.name} - *${menuItem.price}R$*\n`)
+
+    client.sendMessage(chatId, `${textMenu}\n Digite o número da opção desejada.`)
+}
+
+const messagesList = {
+    'menu': (chatId: string) => {
+        handleSendMenu(chatId)
+        return
     },
-    {
-        senderMsg: 'Tudo bem?',
-        responseMsg: 'Tudo e contigo?'
+    'cancelar': (chatId: string) => {
+        delete orders[chatId]
+        client.sendMessage(chatId, `Seu pedido foi cancelado. Obrigado por entrar em contato, esperamos te ver novamente!
+        \n Digite _\'menu\'_ para abrir o menu novamente.`)
+    },
+    'finalizar': (chatId: string) => {
+        client.sendMessage(chatId, 'Sua compra está sendo finalizada. Obrigado por comprar na Vargas!')
+        ongoingChats.splice(ongoingChats.indexOf(chatId), 1)
     }
-]
+}
 
 const menu = [
     { name: 'Corte', price: 55 },
@@ -30,7 +49,6 @@ const menu = [
     { name: 'Combo Barba e Cabelo ', price: 70 },
 ];
 
-const clientOrder = []
 
 client.on('ready', () => {
     console.log('Client is ready!');
@@ -42,42 +60,54 @@ client.on('qr', qr => {
 
 client.on('message_create', (msg) => {
 
+    console.log('Chat ID: ', msg.from);
+    if (msg.fromMe) return;
+    
     // Filtering my phone numbers for testing purposes
     if (msg.from !== process.env.PHONE_NUMBER_SELF && msg.from !== process.env.PHONE_NUMBER_OTHER) return
-    
+    console.log('Orders: ', orders)
+    console.log('ongoing chats: ', ongoingChats)
 
-    if (!hasClientStartedChat) {
-        hasClientStartedChat = true
-        client.sendMessage(msg.from, 'Olá! Vejo que é sua primeira vez entrando em contato conosco! Vou te mandar o menu:')
-
-        let textMenu = ''
-        menu.forEach((menuItem, index) => textMenu += `${index + 1}. ${menuItem.name} - *${menuItem.price}R$*\n`)
-
-        client.sendMessage(msg.from, `${textMenu}\n Digite o número da opção desejada.`)
-        return;
-    }
-
-
-    
     const bodyMsg = msg.body.toLowerCase().trim()
-    console.log(bodyMsg);
+    const chatId = msg.from
 
-    if (bodyMsg === 'cancelar') {
-        clientOrder.length = 0
-        console.log(clientOrder);
-        
+    // Message shown to first-message users
+    if (!ongoingChats.includes(chatId)) {
+        ongoingChats.push(chatId)
+        client.sendMessage(chatId, 'Olá! Vejo que é sua primeira vez entrando em contato conosco! Vou te mandar o menu:')
+
+        handleSendMenu(chatId)
+        return
     }
-    
+
+    Object.entries(messagesList).forEach(([key, value]) => {
+        const action = key
+        const response = value
+
+        if (action === bodyMsg) {
+            response(chatId)
+            return
+        }
+    })
 
     if (Number(bodyMsg) > 0 && Number(bodyMsg) <= menu.length) {
-        console.log(`Opção selecionada: ${bodyMsg}, item correspondente: ${menu[Number(bodyMsg)-1]}`);
-        
-        clientOrder.push(menu[Number(bodyMsg)-1])
+        if (!orders[chatId]) {
+            orders[chatId] = []
+        }
+
+        const selectedOption = menu[Number(bodyMsg)-1]
+
+        if (orders[chatId].some(({ name }) => name === selectedOption.name)) {
+            client.sendMessage(msg.from, 'Esta opção já está na sua lista. Por favor, escolha outra opção ou digite \'_finalizar_\' para prosseguir com o pagamento.')
+            return
+        }
+
+        orders[chatId].push(selectedOption)
         let textClientOrder = ''
-        let totalPrice = clientOrder.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0)
+        let totalPrice = orders[chatId].reduce((accumulator, currentValue) => accumulator + currentValue.price, 0)
         
-        clientOrder.forEach(order => textClientOrder += `• ${order.name} - *${order.price}R$*\n`)
-        client.sendMessage(msg.from, `Sua lista:\n ${textClientOrder}\n Preço total: *${totalPrice}R$* \n _finalizar_ ou _cancelar_`)
+        orders[chatId].forEach(order => textClientOrder += `• ${order.name} - *${order.price}R$*\n`)
+        client.sendMessage(msg.from, `Sua lista:\n${textClientOrder}\n Preço total: *${totalPrice}R$* \n _finalizar_ ou _cancelar_`)
     }
    
     
